ef0e86dc6eb92b9b77b5159acb1506e8496312d0
satoshinnnn <141158586+satoshinnnn@users.noreply.github.com>
2025-03-13 09:28:25 +0700

    Add files via upload

---
+++ bt11.js
@@ -0,0 +1,11 @@
+let a = Math.abs(-5.5);
+let b = Math.abs(10);
+let c = Math.ceil(4.4);
+let d = Math.ceil(5.8);
+let e = Math.floor(5.7);
+let g = Math.round(10.1);
+let l = Math.round(20.6);
+console.log(`a: ${a}, b: ${b},
+     c: ${c}, d: ${d},
+      e: ${e}, g: ${g},
+       l: ${l}`);
